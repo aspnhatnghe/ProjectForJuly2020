299d328aedb67423c52fa2bbd63207a9b59ae3ed
NN <hyhien@gmail.com>
2020-09-19 18:35:08 +0700

    Add KhachHang, DonHang

---
+++ MyProjectForJuly2020/Data/KhachHang.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace MyProjectForJuly2020.Data
+{
+    [Table("KhachHang")]
+    public class KhachHang
+    {
+        [Key]
+        public int MaKh { get; set; }
+        public string HoTen { get; set; }
+        public string SoDienThoai { get; set; }
+        public string Email { get; set; }
+        public string MatKhau { get; set; }
+        public string DiaChi { get; set; }
+        public bool DangHoatDong { get; set; }
+        public string MaNgauNhien { get; set; }
+    }
+}
