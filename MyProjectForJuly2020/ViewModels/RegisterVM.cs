27fd672ea8b292254019f3d15300b2bab3fe1f86
NN <hyhien@gmail.com>
2020-09-19 19:34:17 +0700

    Add encryption extensions

--- MyProjectForJuly2020/ViewModels/RegisterVM.cs
+++ MyProjectForJuly2020/ViewModels/RegisterVM.cs
@@ -6,16 +6,20 @@ namespace MyProjectForJuly2020.ViewModels
     {
         [MaxLength(100)]
         [Required]
+        [Display(Name ="Họ tên")]
         public string HoTen { get; set; }
         [MaxLength(20)]
         [Required]
+        [Display(Name = "Số điện thoại")]
         public string SoDienThoai { get; set; }
         [MaxLength(100)]
         [Required]
         public string Email { get; set; }
         [Required]
+        [DataType(DataType.Password)]
         public string MatKhau { get; set; }
         [Compare("MatKhau", ErrorMessage ="Mật khẩu không khớp")]
+        [DataType(DataType.Password)]
         public string NhapLaiMatKhau { get; set; }
         public string DiaChi { get; set; }
     }
